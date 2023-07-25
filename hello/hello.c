0076e3648e7041212b6ea8f7dd70903e0a92efe5
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-25 11:45:13 +0000

    Tue, Jul 25, 2023, 4:45 AM -07:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-//include to get_string
+// include to get_string
 #include <cs50.h>
-//main method
+// main method
 int main(void)
 {
-    //prompt the user for their name
-    //new string for name = get user input
+    // prompt the user for their name
+    // new string for name = get user input
     string name = get_string("What's your name? ");
-    //prints hello, and %s for the users name input
+    // prints hello, and %s for the users name input
     printf("hello, %s\n", name);
 }
