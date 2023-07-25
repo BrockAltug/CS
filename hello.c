fee827a13831e9b83e5d2bca6ec5d89373c73fba
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-25 09:26:03 +0000

    Tue, Jul 25, 2023, 2:26 AM -07:00

---
+++ hello.c
@@ -0,0 +1,14 @@
+// in terminal $ code hello.c creates a new file named hello in C
+#include <stdio.h>
+
+int main(void)
+{
+    // printf is how to print (print-f is for function)
+    // anytime using String text must be quoted ""
+    // all code ends in ;
+    // \n makes a new line
+    printf("hello world\n");
+}
+// in terminal to compile from source code in C to machine code (binary 0s and 1s)
+// $ make hello
+// $ ./(file name) to run the file
