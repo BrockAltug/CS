8d105855421cdd18742a14aa0a37f62f3e59db8a
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-25 09:47:22 +0000

    Tue, Jul 25, 2023, 2:47 AM -07:00

--- hello.c
+++ hello.c
@@ -9,9 +9,16 @@ int main(void)
     // all code ends in ;
     // \n makes a new line
     printf("hello world\n");
-    
+    // the get_string function is used to get a string from the user
+    // then, the variable answer is passed to the printf function
+    // %s tells the printf function to prepare itself to receive a string
     string answer = get_string("What's your name? ");
+    // answer is a special holding place we call a variable
+    // answer is of type string and can hold any string within it
+    // there are many data types, such as int, bool, char, and many others
     printf("hello, %s\n", answer);
+    //run make hello again in the terminal
+    //then run the updated program
 }
 // in terminal to compile from source code in C to machine code (binary 0s and 1s)
 // $ make hello
