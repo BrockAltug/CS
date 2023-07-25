cf1214716acbbaea9477ef94c2b246cb536c7f51
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-25 12:05:25 +0000

    Tue, Jul 25, 2023, 5:05 AM -07:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,35 +1,42 @@
 #include <cs50.h>
 #include <stdio.h>
 
-//method to get height from user input
-int get_height(void) {
-    //new integer variable for height
+// method to get height from user input
+int get_height(void)
+{
+    // new integer variable for height
     int height;
-    //do while loop
-    do {
-        //get user input for height
+    // do while loop
+    do
+    {
+        // get user input for height
         height = get_int("Height: ");
-        //while height is less than 1 or greater than 8, keep prompting the user
-    } while (height < 1 || height > 8);
-    //return the value of height to the method
+        // while height is less than 1 or greater than 8, keep prompting the user
+    }
+    while (height < 1 || height > 8);
+    // return the value of height to the method
     return height;
 }
 
-//method to draw the pyramid platform shape
-//using int for height
-void draw_pyramid(int height) {
-    //starting from 1 until user input for height, add 1
-    //it is responsible for drawing each level (row) of the pyramid.
-    for (int i = 1; i <= height; i++) {
+// method to draw the pyramid platform shape
+// using int for height
+void draw_pyramid(int height)
+{
+    // starting from 1 until user input for height, add 1
+    // it is responsible for drawing each level (row) of the pyramid.
+    for (int i = 1; i <= height; i++)
+    {
         // draw left pyramid spaces
-        //nested loop starting from 0 until the height - 1, add 1
-        //the number of spaces to draw in each row decreases as the row number increases.
-        for (int j = 0; j < height - i; j++) {
+        // nested loop starting from 0 until the height - 1, add 1
+        // the number of spaces to draw in each row decreases as the row number increases.
+        for (int j = 0; j < height - i; j++)
+        {
             printf(" ");
         }
         // draw left pyramid hashes
         // used to draw the # symbols on the left side of the pyramid.
-        for (int j = 0; j < i; j++) {
+        for (int j = 0; j < i; j++)
+        {
             printf("#");
         }
 
@@ -37,8 +44,9 @@ void draw_pyramid(int height) {
         printf("  ");
 
         // draw right pyramid hashes
-        //used to draw the # symbols on the right side of the pyramid
-        for (int j = 0; j < i; j++) {
+        // used to draw the # symbols on the right side of the pyramid
+        for (int j = 0; j < i; j++)
+        {
             printf("#");
         }
 
@@ -47,9 +55,10 @@ void draw_pyramid(int height) {
     }
 }
 
-int main(void) {
-    //new integer for height calling to the get_height() method to get the height of the level
+int main(void)
+{
+    // new integer for height calling to the get_height() method to get the height of the level
     int height = get_height();
-    //call to the draw_pyramid() method to draw the level using height from the get_height() method
+    // call to the draw_pyramid() method to draw the level using height from the get_height() method
     draw_pyramid(height);
 }
