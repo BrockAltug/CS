1eb4dc18a27983004103cfc2e109abc8cc61d024
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-25 11:50:28 +0000

    Tue, Jul 25, 2023, 4:50 AM -07:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,39 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
-{
-    
+int get_positive_integer(void) {
+    int n;
+    do {
+        n = get_int("Height: ");
+    } while (n < 1 || n > 8);
+
+    return n;
+}
+
+void draw_pyramid(int height) {
+    for (int i = 1; i <= height; i++) {
+        // Draw left pyramid
+        for (int j = 0; j < height - i; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < i; j++) {
+            printf("#");
+        }
+
+        // Draw gap
+        printf("  ");
+
+        // Draw right pyramid
+        for (int j = 0; j < i; j++) {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
+
+int main(void) {
+    int height = get_positive_integer();
+    draw_pyramid(height);
+    return 0;
 }
