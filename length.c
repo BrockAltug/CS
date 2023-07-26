b4b45fe9ec5c7555d3974f161051cfaaffdbf7ae
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 05:57:33 +0000

    Tue, Jul 25, 2023, 10:57 PM -07:00

--- length.c
+++ length.c
@@ -1,4 +1,5 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -23,13 +24,29 @@ int main(void)
     //printf("%i\n", length);
     //---------------------------------------------------------
     //Method 3
+    //string s = get_string("Before: ");
+    //printf("After:  ");
+    //for (int i = 0, n = strlen(s); i < n; i++)
+    //{
+    //    if (s[i] >= 'a' && s[i] <= 'z')
+    //    {
+    //        printf("%c", s[i] - 32);
+    //    }
+    //    else
+    //    {
+    //        printf("%c", s[i]);
+    //    }
+    //}
+    //printf("\n");
+    //-----------------------------------------------------------
+    //Method 4
     string s = get_string("Before: ");
     printf("After:  ");
     for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if (s[i] >= 'a' && s[i] <= 'z')
+        if (islower(s[i]))
         {
-            printf("%c", s[i] - 32);
+            printf("%c", toupper(s[i]));
         }
         else
         {
