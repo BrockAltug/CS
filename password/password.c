c86f01292e43e01e83d48bc62c1d73646283b3bf
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 07:42:14 +0000

    Wed, Jul 26, 2023, 12:42 AM -07:00

--- password/password.c
+++ password/password.c
@@ -1,70 +1,73 @@
-// Include necessary header files
 #include <stdio.h>
 #include <cs50.h>
 #include <ctype.h>
-#include <stdbool.h> // Add this line to include the necessary header
+#include <stdbool.h>
 
-// Function prototype
+// method if the password is valid true or false
 bool valid(string password);
 
-// Main function
+// Main method
 int main(void)
 {
-    // Get password input from the user
+    // get password input from the user
     string password = get_string("Enter your password: ");
 
-    // Check if the password is valid using the valid function
+    // check if the password is valid using the valid function
+    //if the password is valid
     if (valid(password))
     {
         printf("Your password is valid!\n");
     }
+    //else the password doesnt match the criteria
     else
     {
         printf("Your password needs at least one uppercase letter, lowercase letter, number, and symbol\n");
     }
 }
 
-// Function to check if the password meets the required criteria
+// method to check if the password meets the required criteria
 bool valid(string password)
 {
-    // Boolean variables to track whether we found a lowercase letter, uppercase letter, number, and symbol
+    // boolean variables to track whether we found a lowercase letter, uppercase letter, number, and symbol
     bool has_lower = false;
     bool has_upper = false;
     bool has_digit = false;
     bool has_symbol = false;
 
-    // Iterate through each character in the password
+    // go through each character in the password
     for (int i = 0; password[i] != '\0'; i++)
     {
         char c = password[i];
 
-        // Check if the character is a lowercase letter
+        // check if the character is a lowercase letter
         if (islower(c))
         {
             has_lower = true;
         }
-        // Check if the character is an uppercase letter
+        // check if the character is an uppercase letter
         else if (isupper(c))
         {
             has_upper = true;
         }
-        // Check if the character is a digit (number)
+        // check if the character is a digit (number)
         else if (isdigit(c))
         {
             has_digit = true;
         }
-        // Check if the character is a symbol (non-alphanumeric)
+        // check if the character is a symbol (non-alphanumeric)
         else if (ispunct(c))
         {
             has_symbol = true;
         }
     }
 
-    // Check if all criteria are met (has at least one lowercase letter, one uppercase letter, one number, and one symbol)
+    // check if all criteria are met (has at least one lowercase letter, one uppercase letter, one number, and one symbol)
     if (has_lower && has_upper && has_digit && has_symbol)
     {
+        //return true to valid if it meets criteria
         return true;
     }
+    //else return false 
     else
     {
         return false;
