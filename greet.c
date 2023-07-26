f5689ce6c048586ed75f6e54fd851d1353c99ca7
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 06:05:03 +0000

    Tue, Jul 25, 2023, 11:05 PM -07:00

--- greet.c
+++ greet.c
@@ -1,8 +1,36 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
+//Method 1
+//int main(void)
+//{
+//    string name = get_string("What's your name? ");
+//    printf("hello, %s\n", name);
+//}
+//-----------------------------------------------------
+//Method 2
+//int main(int argc, string argv[])
+//{
+//    if (argc == 2)
+//    {
+//        printf("hello, %s\n", argv[1]);
+//    }
+//    else
+//    {
+//        printf("hello, world\n");
+//    }
+//}
+//-------------------------------------------------------
+//Method 3
+int main(int argc, string argv[])
 {
-    string name = get_string("What's your name? ");
-    printf("hello, %s\n", name);
+    if (argc != 2)
+    {
+        printf("Missing command-line argument\n");
+        return 1;
+    }
+    printf("hello, %s\n", argv[1]);
+    return 0;
 }
+//-------------------------------------------------------
+
