ddd0d1157e27caa1dd760b1d099f1601319efc6d
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 05:55:47 +0000

    Tue, Jul 25, 2023, 10:55 PM -07:00

--- length.c
+++ length.c
@@ -1,16 +1,40 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
+    //Method 1
     // Prompt for user's name
-    string name = get_string("Name: ");
+    //string name = get_string("Name: ");
 
     // Count number of characters up until '\0' (aka NUL)
-    int n = 0;
-    while (name[n] != '\0')
+    //int n = 0;
+    //while (name[n] != '\0')
+    //{
+    //    n++;
+    //}
+    //printf("%i\n", n);
+    //--------------------------------------------------------
+    //Method 2
+    // Prompt for user's name
+    //string name = get_string("Name: ");
+    //int length = strlen(name);
+    //printf("%i\n", length);
+    //---------------------------------------------------------
+    //Method 3
+    string s = get_string("Before: ");
+    printf("After:  ");
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
-        n++;
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            printf("%c", s[i] - 32);
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
     }
-    printf("%i\n", n);
+    printf("\n");
 }
