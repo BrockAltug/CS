f7a2323441097fb08bab5968753122e0d8d7cbca
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 06:59:55 +0000

    Tue, Jul 25, 2023, 11:59 PM -07:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,89 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(void) 
+// Function prototypes
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+int calculate_grade_level(int letters, int words, int sentences);
+
+int main(void)
 {
+    // Get input text from the user
+    string text = get_string("Text: ");
 
+    // Count the number of letters, words, and sentences in the text
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
+
+    // Calculate the grade level using the Coleman-Liau index formula
+    int grade_level = calculate_grade_level(letters, words, sentences);
+
+    // Print the grade level
+    if (grade_level < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (grade_level >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade_level);
+    }
+}
+
+// Function to count the number of letters in the text
+int count_letters(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+// Function to count the number of words in the text
+int count_words(string text)
+{
+    int count = 1;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isspace(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+// Function to count the number of sentences in the text
+int count_sentences(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+// Function to calculate the grade level using the Coleman-Liau index formula
+int calculate_grade_level(int letters, int words, int sentences)
+{
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+    float index = 0.0588 * L - 0.296 * S - 15.8;
+    return (int) (index + 0.5); // Round the index to the nearest integer
 }
