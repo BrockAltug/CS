7df556fba987495614a1d0f20e113f151dedad61
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 07:24:09 +0000

    Wed, Jul 26, 2023, 12:24 AM -07:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,27 +1,35 @@
-#include <ctype.h> // Include the necessary header file for character manipulation functions
-#include <cs50.h> // Include the necessary header file for using CS50 functions
-#include <stdio.h> // Include the necessary header file for standard input/output functions
-#include <string.h> // Include the necessary header file for string manipulation functions
+// Include the necessary header file for character manipulation functions
+#include <ctype.h>
+// Include the necessary header file for using CS50 functions
+#include <cs50.h>
+// Include the necessary header file for standard input/output functions
+#include <stdio.h>
+// Include the necessary header file for string manipulation functions
+#include <string.h>
 
 // Function prototypes
-bool is_valid_key(string key); // Function to validate the key provided by the user
-string encrypt(string plaintext, string key); // Function to encrypt the plaintext using the key
+// Function to validate the key provided by the user
+bool is_valid_key(string key);
+// Function to encrypt the plaintext using the key
+string encrypt(string plaintext, string key);
 
 int main(int argc, string argv[])
 {
     // Check for the correct number of command-line arguments
     if (argc != 2)
     {
+        // Print usage message and return 1 to signify an error
         printf("Usage: %s key\n", argv[0]);
-        return 1; // Return 1 to signify an error
+        return 1;
     }
 
     // Validate the key provided by the user
     string key = argv[1];
     if (!is_valid_key(key))
     {
+        // Print error message and return 1 to signify an error
         printf("Key must contain 26 characters.\n");
-        return 1; // Return 1 to signify an error
+        return 1;
     }
 
     // Get plaintext input from the user
@@ -33,13 +41,16 @@ int main(int argc, string argv[])
     // Print the ciphertext
     printf("ciphertext: %s\n", ciphertext);
 
-    return 0; // Return 0 to indicate successful execution
+    // Return 0 to indicate successful execution
+    return 0;
 }
 
 // Function to validate the key
 bool is_valid_key(string key)
 {
+    // Get the length of the key
     int key_length = strlen(key);
+    // Check if the key length is not 26 characters
     if (key_length != 26)
     {
         return false;
@@ -48,47 +59,64 @@ bool is_valid_key(string key)
     // Initialize an array to keep track of characters in the key
     bool characters[26] = {false};
 
+    // Iterate over each character in the key
     for (int i = 0; i < key_length; i++)
     {
-        char c = toupper(key[i]); // Convert the key character to uppercase
-        if (!isalpha(c)) // Check if the character is not an alphabet
+        // Convert the key character to uppercase
+        char c = toupper(key[i]);
+        // Check if the character is not an alphabet
+        if (!isalpha(c))
         {
             return false;
         }
 
-        int index = c - 'A'; // Calculate the index of the character in the alphabet
-        if (characters[index]) // Check if the character has already been used in the key
+        // Calculate the index of the character in the alphabet
+        int index = c - 'A';
+        // Check if the character has already been used in the key
+        if (characters[index])
         {
             return false;
         }
-        characters[index] = true; // Mark the character as used in the key
+        // Mark the character as used in the key
+        characters[index] = true;
     }
 
-    return true; // Return true if the key is valid
+    // Return true if the key is valid
+    return true;
 }
 
 // Function to encrypt the plaintext using the key
 string encrypt(string plaintext, string key)
 {
-    string ciphertext = plaintext; // Initialize ciphertext with plaintext
+    // Initialize ciphertext with plaintext
+    string ciphertext = plaintext;
+    // Get the length of the plaintext
     int length = strlen(plaintext);
 
+    // Iterate over each character in the plaintext
     for (int i = 0; i < length; i++)
     {
-        char c = plaintext[i]; // Get each character in the plaintext
-        if (isalpha(c)) // Check if the character is an alphabet
+        // Get each character in the plaintext
+        char c = plaintext[i];
+        // Check if the character is an alphabet
+        if (isalpha(c))
         {
-            int index = toupper(c) - 'A'; // Calculate the index of the character in the alphabet
-            if (islower(c)) // Check if the character is lowercase
+            // Calculate the index of the character in the alphabet
+            int index = toupper(c) - 'A';
+            // Check if the character is lowercase
+            if (islower(c))
             {
-                ciphertext[i] = tolower(key[index]); // Encrypt the character as lowercase
+                // Encrypt the character as lowercase
+                ciphertext[i] = tolower(key[index]);
             }
             else
             {
-                ciphertext[i] = toupper(key[index]); // Encrypt the character as uppercase
+                // Encrypt the character as uppercase
+                ciphertext[i] = toupper(key[index]);
             }
         }
     }
 
-    return ciphertext; // Return the encrypted ciphertext
+    // Return the encrypted ciphertext
+    return ciphertext;
 }
