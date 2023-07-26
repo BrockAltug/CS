17037f1d3c9f6bd40a503983a97d81a3047d690c
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 01:36:03 +0000

    Tue, Jul 25, 2023, 6:36 PM -07:00

--- hi.c
+++ hi.c
@@ -1,12 +1,22 @@
+#include <cs50.h>
 #include <stdio.h>
+
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
+    //char c1 = 'H';
+    //char c2 = 'I';
+    //char c3 = '!';
 
-    printf("%i %i %i\n", c1, c2, c3);
+    //printf("%i %i %i\n", c1, c2, c3);
+    //===========================================
+    //string s = "HI!";
+    //printf("%i %i %i\n", s[0], s[1], s[2]);
+    //===========================================
     string s = "HI!";
-    printf("%i %i %i\n", s[0], s[1], s[2]);
+    string t = "BYE!";
+
+    printf("%s\n", s);
+    printf("%s\n", t);
+
 }
