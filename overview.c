9dcecf05a1fea9041201879dd9c7231cbd1382a6
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 06:25:05 +0000

    Tue, Jul 25, 2023, 11:25 PM -07:00

--- overview.c
+++ overview.c
@@ -1,3 +1,43 @@
+//Linux and the Command Line
+//Linux is an operating system that is accessible via the command line in the terminal window in VS Code.
+//Some common command-line arguments we may use include:
+//cd - for changing our current directory (folder)
+//cp - for copying files and directories
+//ls - for listing files in a directory
+//mkdir - for making a directory
+//mv - for moving (renaming) files and directories
+//rm - for removing (deleting) files
+//rmdir - for removing (deleting) directories
+//The most commonly used is ls which will list all the files in the current directory or directory.
+//Go ahead and type ls into the terminal window and hit enter. You’ll see all the files in the current folder.
+//Another useful command is mv, where you can move a file from one file to another. For example,
+//you could use this command to rename Hello.c (notice the uppercase H) to hello.c by typing mv Hello.c hello.c.
+//You can also create folders. You can type mkdir pset1 to create a directory called pset1.
+//You can then use cd pset1 to change your current directory to pset1.
+//-----------------------------------------------------------------------------
+// Operators and Types
+// Operators refer to the mathematical operations that are supported by your compiler.
+// In C, these mathematical operators include:
+
+// + for addition
+// - for subtraction
+// * for multiplication
+// / for division
+// % for remainder
+// Types refer to the possible data that can be stored within a variable.
+// For example, a char is designed to accommodate a single character like a or 2.
+// Types are very important because each type has specific limits.
+// For example, because of the limits in memory, the highest value of an int can be 4294967296.
+// Types with which you might interact during this course include:
+
+// bool, a Boolean expression of either true or false
+// char, a single character like a or 2
+// double, a floating-point value with more digits than a float
+// float, a floating-point value, or real number with a decimal value
+// int, integers up to a certain size, or number of bits
+// long, integers with more bits, so they can count higher than an int
+// string, a string of characters
+//-----------------------------------------------------------------------------
 //Things Discussed -
 //Week 1
 //How to create your first program in C.
