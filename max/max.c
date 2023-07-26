bfe9fc5c390457873d90aa46b6dbfc8295ae9fb3
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 11:51:20 +0000

    Wed, Jul 26, 2023, 4:51 AM -07:00

--- max/max.c
+++ max/max.c
@@ -1,8 +1,7 @@
-// Practice writing a function to find a max value
-
 #include <cs50.h>
 #include <stdio.h>
 
+// method to find the max value in an array
 int max(int array[], int n);
 
 int main(void)
@@ -10,22 +9,39 @@ int main(void)
     int n;
     do
     {
+        // Get the number of elements from the user
         n = get_int("Number of elements: ");
-    } 
+    }
     while (n < 1);
 
     int arr[n];
 
+    // get elements from the user and store them in the array
     for (int i = 0; i < n; i++)
     {
         arr[i] = get_int("Element %i: ", i);
     }
 
+    // find and print the max value in the array
     printf("The max value is %i.\n", max(arr, n));
 }
 
-// TODO: return the max value
+// method to find the max value in an array
 int max(int array[], int n)
 {
-    return 0;
+    // initialize max value as the first element in the array
+    int max_value = array[0];
+
+    // go through the array to find the max value
+    for (int i = 1; i < n; i++)
+    {
+        // update max_value if a larger element is found
+        if (array[i] > max_value)
+        {
+            max_value = array[i];
+        }
+    }
+
+    // return the max value
+    return max_value;
 }
