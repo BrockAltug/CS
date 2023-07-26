d7c640cfffbb39a11c07cbed78c5a5f56eb39a71
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 06:13:41 +0000

    Tue, Jul 25, 2023, 11:13 PM -07:00

--- greet.c
+++ greet.c
@@ -1,24 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
-//Method 1
-//int main(void)
-//{
-//    string name = get_string("What's your name? ");
-//    printf("hello, %s\n", name);
-//}
+// Method 1: A simple program to greet the user by name
+// int main(void)
+// {
+//     // Prompt the user to input their name
+//     string name = get_string("What's your name? ");
+//     // Print a greeting message with the provided name
+//     printf("hello, %s\n", name);
+// }
 //-----------------------------------------------------
-//Method 2
+
+// Method 2: A program that prints a greeting with a command-line argument or "hello, world" if none is provided
 int main(int argc, string argv[])
 {
+    // check if there is exactly one command-line argument (program name is argc[0])
     if (argc == 2)
     {
+        // print a greeting message with the provided command-line argument
         printf("hello, %s\n", argv[1]);
     }
     else
     {
+        // print a generic greeting message as no command-line argument was provided
         printf("hello, world\n");
     }
 }
-
-
--- overview.c
+++ overview.c
@@ -1,4 +1,5 @@
 //Things Discussed -
+//Week 1
 //How to create your first program in C.
 //Predefined functions that come natively with C and how to implement your own functions.
 //How to use variables, conditionals, and loops.
@@ -7,3 +8,16 @@
 //How to integrate comments into your code.
 //How to approach abstraction to simplify and improve your code.
 //How to utilize types and operators.
+//-----------------------------------------------------------------------------
+//Week 2
+//learned more details about compiling and how data is stored within a computer. Specifically, you learned…
+//Generally, how a compiler works.
+//How to debug your code using four methods.
+//How to utilize arrays within your code.
+//How arrays store data in back to back portions of memory.
+//How strings are simply arrays of characters.
+//How to interact with arrays in your code.
+//How command-line arguments can be passed to your programs.
+//The basic building-blocks of cryptography.
+//-----------------------------------------------------------------------------
+//Week 3
