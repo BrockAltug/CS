f211f46eb287b2b0812493f9b4a75d0ef47c11f7
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 05:52:23 +0000

    Tue, Jul 25, 2023, 10:52 PM -07:00

--- hi.c
+++ hi.c
@@ -1,24 +1,32 @@
+// Include the cs50 library for string data type and get_string() function
 #include <cs50.h>
+// Include the standard input/output library
 #include <stdio.h>
-
 
 int main(void)
 {
-    //char c1 = 'H';
-    //char c2 = 'I';
-    //char c3 = '!';
+    // Method 1: Using individual characters
+    // char c1 = 'H';
+    // char c2 = 'I';
+    // char c3 = '!';
 
-    //printf("%i %i %i\n", c1, c2, c3);
+    // printf("%i %i %i\n", c1, c2, c3);
     //===========================================
-    //string s = "HI!";
-    //printf("%i %i %i\n", s[0], s[1], s[2]);
-    //===========================================
-    //string s = "HI!";
-    //string t = "BYE!";
 
-    //printf("%s\n", s);
-    //printf("%s\n", t);
+    // Method 2: Using a string
+    // string s = "HI!";
+    // printf("%i %i %i\n", s[0], s[1], s[2]);
     //===========================================
+
+    // Method 3: Using multiple strings
+    // string s = "HI!";
+    // string t = "BYE!";
+
+    // printf("%s\n", s);
+    // printf("%s\n", t);
+    //===========================================
+
+    // Method 4: Using an array of strings
     string words[2];
 
     words[0] = "HI!";
@@ -26,5 +34,4 @@ int main(void)
 
     printf("%s\n", words[0]);
     printf("%s\n", words[1]);
-
 }
