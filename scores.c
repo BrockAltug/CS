26ba873a2ea3e4cbf6714d5933030b0b374c3710
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 01:08:58 +0000

    Tue, Jul 25, 2023, 6:08 PM -07:00

--- scores.c
+++ scores.c
@@ -4,23 +4,23 @@
 int main(void)
 {
     // Scores
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    //int score1 = 72;
+    //int score2 = 73;
+    //int score3 = 33;
 
     // Print average
-    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+    //printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
     //-------------------------------------------------------------------
     // Scores
-    int scores[3];
-    scores[0] = 72;
-    scores[1] = 73;
-    scores[2] = 33;
+    //int scores[3];
+    //scores[0] = 72;
+    //scores[1] = 73;
+    //scores[2] = 33;
 
     // Print average
-    printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
+    //printf("Average: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
     //--------------------------------------------------------------------
-    // Get scores
+     // Get scores
     int scores[3];
     for (int i = 0; i < 3; i++)
     {
