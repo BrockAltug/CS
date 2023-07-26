7d00bfdee36ad960b5061fd06e436eb19ddab5ff
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 05:46:17 +0000

    Tue, Jul 25, 2023, 10:46 PM -07:00

--- hi.c
+++ hi.c
@@ -13,10 +13,11 @@ int main(void)
     //string s = "HI!";
     //printf("%i %i %i\n", s[0], s[1], s[2]);
     //===========================================
-    string s = "HI!";
-    string t = "BYE!";
+    
+    //string s = "HI!";
+    //string t = "BYE!";
 
-    printf("%s\n", s);
-    printf("%s\n", t);
+    //printf("%s\n", s);
+    //printf("%s\n", t);
 
 }
