f05c58717d2e9c3d9da4304f1c53a325dd52af96
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 06:52:54 +0000

    Tue, Jul 25, 2023, 11:52 PM -07:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -34,29 +34,29 @@ string replace(string input)
         // use a switch statement to replace vowels with numbers
         switch (ch)
         {
-            //incase if a
+            // incase if a
             case 'a':
-            //convert to 6
+                // convert to 6
                 converted_word[i] = '6';
                 break;
-                //in case if e
+                // in case if e
             case 'e':
-            //convert to 3
+                // convert to 3
                 converted_word[i] = '3';
                 break;
-                //in case if i
+                // in case if i
             case 'i':
-            //convert to 1
+                // convert to 1
                 converted_word[i] = '1';
                 break;
-                //in case if o
+                // in case if o
             case 'o':
-            //convert to 0
+                // convert to 0
                 converted_word[i] = '0';
                 break;
-            // For 'u', we don't change anything, so no need to include a case for it.
+                // For 'u', we don't change anything, so no need to include a case for it.
         }
     }
-    //return the word after vowel conversion
+    // return the word after vowel conversion
     return converted_word;
 }
