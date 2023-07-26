4c51225a2c5aec351d626de8bef364b73fc7667d
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-26 11:30:41 +0000

    Wed, Jul 26, 2023, 4:30 AM -07:00

--- search.c
+++ search.c
@@ -47,55 +47,25 @@
 // }
 //---------------------------------------------------------------------
 // Method 3: Searching for a name and corresponding number in arrays of strings
-// int main(void)
-//{
-// Arrays of strings
-//    string names[] = {"Carter", "David"};
-//    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
-
-// Get input name from the user
-//    string name = get_string("Name: ");
-
-// Search for the name and corresponding number in the array
-//    for (int i = 0; i < 2; i++)
-//    {
-//        if (strcmp(names[i], name) == 0)
-//        {
-//            printf("Found %s\n", numbers[i]);
-//            return 0;
-//        }
-//    }
-//    printf("Not found\n");
-//    return 1;
-//}
-//---------------------------------------------------------------------
-// Method 4: Using a struct to store name and number and searching for a name
-typedef struct
+ int main(void)
 {
-    string name;
-    string number;
-} person;
+ Arrays of strings
+    string names[] = {"Carter", "David"};
+    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
 
-int main(void)
-{
-    person people[2];
-
-    people[0].name = "Carter";
-    people[0].number = "+1-617-495-1000";
-
-    people[1].name = "David";
-    people[1].number = "+1-949-468-2750";
-
-    // Search for name
+ Get input name from the user
     string name = get_string("Name: ");
+
+ Search for the name and corresponding number in the array
     for (int i = 0; i < 2; i++)
     {
-        if (strcmp(people[i].name, name) == 0)
+        if (strcmp(names[i], name) == 0)
         {
-            printf("Found %s\n", people[i].number);
+            printf("Found %s\n", numbers[i]);
             return 0;
         }
     }
     printf("Not found\n");
     return 1;
 }
+
