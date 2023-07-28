d1c571d5f47ced6fe0946ff0d7fd2c21da27c50e
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-28 07:38:24 +0000

    Fri, Jul 28, 2023, 12:38 AM -07:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -1,16 +1,17 @@
 #include "helpers.h"
 
+// colorize method
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Iterate over each pixel in the image
+    // go over each pixel in the image
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            // Check if the pixel is black (all RGB components are 0)
+            // check if the pixel is black (all RGB components are 0)
             if (image[i][j].rgbtRed == 0 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtBlue == 0)
             {
-                // Set the pixel to red (full red intensity, no green or blue)
+                // set the pixel to red (max value red, no green or blue)
                 image[i][j].rgbtRed = 255;
                 image[i][j].rgbtGreen = 0;
                 image[i][j].rgbtBlue = 0;
