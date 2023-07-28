6d2a5348c76d0dd04e54efb7126dfe11a8575fe4
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-28 02:53:47 +0000

    Thu, Jul 27, 2023, 7:53 PM -07:00

--- addresses.c
+++ addresses.c
@@ -1,7 +1,25 @@
+#include <cs50.h>
 #include <stdio.h>
 
+// int main(void)
+//{
+//     int n = 50;
+//     int *p = &n;
+//     printf("%i\n", *p);
+// }
+//-----------------------------
+// int main(void)
+//{
+//    string s = "HI!";
+//    printf("%p\n", s);
+//    printf("%p\n", &s[0]);
+//    printf("%p\n", &s[1]);
+//    printf("%p\n", &s[2]);
+//    printf("%p\n", &s[3]);
+// }
+//-----------------------------
 int main(void)
 {
-    int n = 50;
-    printf("%p\n", &n);
+    char *s = "HI!";
+    printf("%s\n", s);
 }
