2fe2892ea5b3b68e226a9190c29dd7867fc61a86
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-07-28 07:36:18 +0000

    Fri, Jul 28, 2023, 12:36 AM -07:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,5 +2,19 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Change all black pixels to a color of your choosing
+    // Iterate over each pixel in the image
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            // Check if the pixel is black (all RGB components are 0)
+            if (image[i][j].rgbtRed == 0 && image[i][j].rgbtGreen == 0 && image[i][j].rgbtBlue == 0)
+            {
+                // Set the pixel to red (full red intensity, no green or blue)
+                image[i][j].rgbtRed = 255;
+                image[i][j].rgbtGreen = 0;
+                image[i][j].rgbtBlue = 0;
+            }
+        }
+    }
 }
