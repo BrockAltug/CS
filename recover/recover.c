888294edb94d34793a62dcdec45ae2698241cc80
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-01 11:15:00 +0000

    Tue, Aug 1, 2023, 4:15 AM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,65 +1,74 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 
 #define BLOCK_SIZE 512
 
 typedef uint8_t BYTE;
 
-int main(int argc, char *argv[]) {
-    // Check command-line arguments
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    // check command-line arguments
+    if (argc != 2)
+    {
         printf("Usage: %s <forensic_image>\n", argv[0]);
         return 1;
     }
 
-    // Open the forensic image
+    // open the forensic image
     FILE *forensic_image = fopen(argv[1], "r");
-    if (forensic_image == NULL) {
+    if (forensic_image == NULL)
+    {
         printf("Could not open the forensic image.\n");
         return 1;
     }
 
-    // Initialize variables for JPEG recovery
+    // initialize variables for JPEG recovery
     BYTE buffer[BLOCK_SIZE];
     int jpeg_counter = 0;
     FILE *jpeg_file = NULL;
     char filename[8];
 
-    // Read the forensic image block by block
-    while (fread(buffer, sizeof(BYTE), BLOCK_SIZE, forensic_image) == BLOCK_SIZE) {
-        // Check for the start of a new JPEG
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0) {
-            // Close the previous JPEG file (if any)
-            if (jpeg_file != NULL) {
+    // read the forensic image block by block
+    while (fread(buffer, sizeof(BYTE), BLOCK_SIZE, forensic_image) == BLOCK_SIZE)
+    {
+        // check for the start of a new JPEG
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            // close the previous JPEG file (if any)
+            if (jpeg_file != NULL)
+            {
                 fclose(jpeg_file);
             }
 
-            // Create a new JPEG file with the appropriate filename
+            // create a new JPEG file with the appropriate filename
             snprintf(filename, sizeof(filename), "%03d.jpg", jpeg_counter);
             jpeg_file = fopen(filename, "w");
-            if (jpeg_file == NULL) {
+            if (jpeg_file == NULL)
+            {
                 printf("Could not create the output JPEG file.\n");
                 fclose(forensic_image);
                 return 1;
             }
 
-            // Increment the JPEG counter
+            // increment the JPEG counter
             jpeg_counter++;
         }
 
-        // Write the block to the current JPEG file (if it exists)
-        if (jpeg_file != NULL) {
+        // write the block to the current JPEG file (if it exists)
+        if (jpeg_file != NULL)
+        {
             fwrite(buffer, sizeof(BYTE), BLOCK_SIZE, jpeg_file);
         }
     }
 
-    // Close the last JPEG file (if any)
-    if (jpeg_file != NULL) {
+    // close the last JPEG file (if any)
+    if (jpeg_file != NULL)
+    {
         fclose(jpeg_file);
     }
 
-    // Close the forensic image
+    // close the forensic image
     fclose(forensic_image);
 
     return 0;
