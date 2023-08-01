982ce469a4e29fc373762eee12968361c0c4f4c4
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-01 11:02:28 +0000

    Tue, Aug 1, 2023, 4:02 AM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-int main(int argc, char *argv[])
-{
- 
+
+#define BLOCK_SIZE 512
+
+typedef struct {
+    char filename[8];
+    FILE *fp;
+} JPEGFile;
+
+int main(int argc, char *argv[]) {
+    // Check command-line arguments
+    if (argc != 2) {
+        printf("Usage: %s <forensic_image>\n", argv[0]);
+        return 1;
+    }
+
+    // Open the forensic image
+    FILE *forensic_image = fopen(argv[1], "r");
+    if (forensic_image == NULL) {
+        printf("Could not open the forensic image.\n");
+        return 1;
+    }
+
+    int jpeg_counter = 0;
+    JPEGFile *jpeg_files = NULL;
+
+    unsigned char block[BLOCK_SIZE];
+    while (fread(block, BLOCK_SIZE, 1, forensic_image)) {
+        // Check for JPEG signatures
+        if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && (block[3] & 0xf0) == 0xe0) {
+            // Close the previous file (if any)
+            if (jpeg_counter > 0) {
+                fclose(jpeg_files[jpeg_counter - 1].fp);
+            }
+
+            // Create a new JPEG file
+            snprintf(jpeg_files[jpeg_counter].filename, sizeof(jpeg_files[jpeg_counter].filename), "%03d.jpg", jpeg_counter);
+            jpeg_files[jpeg_counter].fp = fopen(jpeg_files[jpeg_counter].filename, "w");
+            if (jpeg_files[jpeg_counter].fp == NULL) {
+                printf("Could not create the output JPEG file.\n");
+                fclose(forensic_image);
+                free(jpeg_files);
+                return 1;
+            }
+
+            jpeg_counter++;
+            jpeg_files = realloc(jpeg_files, jpeg_counter * sizeof(JPEGFile));
+            if (jpeg_files == NULL) {
+                printf("Memory allocation failed.\n");
+                fclose(forensic_image);
+                return 1;
+            }
+        }
+
+        // Write the block to the current JPEG file
+        if (jpeg_counter > 0) {
+            fwrite(block, BLOCK_SIZE, 1, jpeg_files[jpeg_counter - 1].fp);
+        }
+    }
+
+    // Close the last JPEG file (if any)
+    if (jpeg_counter > 0) {
+        fclose(jpeg_files[jpeg_counter - 1].fp);
+    }
+
+    // Free allocated memory
+    free(jpeg_files);
+
+    // Close the forensic image
+    fclose(forensic_image);
+
+    return 0;
 }
