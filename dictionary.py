4bdaf9362a6967a78fbe07782d43c0de5e409dc3
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-07 23:33:24 +0000

    Mon, Aug 7, 2023, 4:33 PM -07:00

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+words = set()
+
+def check(word):
+    if word.lower() in words:
+        return True
+    else:
+        return False
+
+
+
+
+def load(dictionary):
+    file = open(dictionary, "r")
+    for line in file:
+        words.add(line)
+        close(file)
+        return True
+
+
+
+
+
+def size():
+    ...
+
+
+
+def unload():
+    ...
+
