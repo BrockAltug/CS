dfc497e7e0e1f115e6444b9634694f6c5c48aa6a
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-12 10:59:58 +0000

    Sat, Aug 12, 2023, 3:59 AM -07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,6 +1,10 @@
+# import the get_string function from the cs50 library
 from cs50 import get_string
 
+
+# Main method
 def main():
+    # prompt the user to input some text
     text = get_string("Text: ")
     letters, words, sentences = count_elements(text)
     L = letters / words * 100
@@ -9,15 +13,21 @@ def main():
     grade_level = get_grade_level(index)
     print(grade_level)
 
+
+# method to count letters, words, and sentences in the given text
 def count_elements(text):
     letters = sum(1 for char in text if char.isalpha())
     words = len(text.split())
-    sentences = sum(1 for char in text if char in ['.', '!', '?'])
+    sentences = sum(1 for char in text if char in [".", "!", "?"])
     return letters, words, sentences
 
+
+# method to compute the Coleman-Liau index
 def compute_index(L, S):
     return 0.0588 * L - 0.296 * S - 15.8
 
+
+# method to get the corresponding grade level
 def get_grade_level(index):
     rounded_index = round(index)
     if rounded_index >= 16:
@@ -27,5 +37,8 @@ def get_grade_level(index):
     else:
         return f"Grade {rounded_index}"
 
+
+# check if the script is being run as the main program
 if __name__ == "__main__":
+    # call the main method to start the program
     main()
