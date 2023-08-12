038fcc9aa051bd53a7f6b98564ecbee54c493f2e
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-12 02:49:28 +0000

    Fri, Aug 11, 2023, 7:49 PM -07:00

---
+++ greet.py
@@ -0,0 +1,22 @@
+# Prints a command-line argument
+
+from sys import argv
+
+if len(argv) == 2:
+    print(f"hello, {argv[1]}")
+else:
+    print("hello, world")
+#--------------------------------------
+# Printing command-line arguments, indexing into argv
+
+from sys import argv
+
+for i in range(len(argv)):
+    print(argv[i])
+#-------------------------------------
+# Printing command-line arguments using a slice
+
+from sys import argv
+
+for arg in argv[1:]:
+    print(arg)
