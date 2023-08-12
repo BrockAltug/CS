cdcadbd92a76a106a152cc3d443e25ae2587ddc0
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-12 10:04:41 +0000

    Sat, Aug 12, 2023, 3:04 AM -07:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,4 +1,4 @@
-# Menu of entrees with their prices
+# menu of entrees with their prices
 menu = {
     "Baja Taco": 4.00,
     "Burrito": 7.50,
@@ -11,21 +11,22 @@ menu = {
     "Tortilla Salad": 8.00
 }
 
-# Function to calculate the total cost of the order
+# method to calculate the total cost of the order
 def calculate_total(order):
     total = sum(menu[item] for item in order)
     return total
 
-# Main function to place an order
+# Main method to place an order
 def main():
     print("Welcome to Felipe's Taqueria!")
     print("Please enter your order, one item per line.")
     print("Press Ctrl-D (Ctrl-Z on Windows) to finish your order.\n")
 
-    order = []  # List to store the user's order
+    order = []  # list to store the user's order
 
     try:
         while True:
+            # prompt the user to input an item
             item = input("Item: ").title()  # Convert the input to title case
             if item in menu:
                 order.append(item)
@@ -34,7 +35,7 @@ def main():
             else:
                 print("Invalid item. Please choose from the menu.\n")
     except EOFError:
-        # Handle the Ctrl-D (or Ctrl-Z on Windows) input to end the order
+        # handle the Ctrl-D (or Ctrl-Z on Windows) input to end the order
         total = calculate_total(order)
         print(f"Final Total: ${total:.2f}")
         print("Thank you for your order!")
