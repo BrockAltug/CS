cfbe5e85ef5aa27f0130065bc3cd4067aa3e3e11
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-12 10:09:02 +0000

    Sat, Aug 12, 2023, 3:09 AM -07:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -1,29 +1,33 @@
-# Simulate a sports tournament
-
 import csv
 import sys
 import random
+import time
 
-# Number of simluations to run
+# Number of simulations to run
 N = 1000
 
-
 def main():
-
     # Ensure correct usage
     if len(sys.argv) != 2:
         sys.exit("Usage: python tournament.py FILENAME")
 
     teams = []
-    # TODO: Read teams into memory from file
+    # Read teams into memory from file
+    filename = sys.argv[1]
+    with open(filename) as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            teams.append({"team": row["team"], "rating": int(row["rating"])})
 
     counts = {}
-    # TODO: Simulate N tournaments and keep track of win counts
+    # Simulate N tournaments and keep track of win counts
+    for _ in range(N):
+        winner = simulate_tournament(teams)
+        counts[winner] = counts.get(winner, 0) + 1
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
         print(f"{team}: {counts[team] * 100 / N:.1f}% chance of winning")
-
 
 def simulate_game(team1, team2):
     """Simulate a game. Return True if team1 wins, False otherwise."""
@@ -31,7 +35,6 @@ def simulate_game(team1, team2):
     rating2 = team2["rating"]
     probability = 1 / (1 + 10 ** ((rating2 - rating1) / 600))
     return random.random() < probability
-
 
 def simulate_round(teams):
     """Simulate a round. Return a list of winning teams."""
@@ -46,11 +49,11 @@ def simulate_round(teams):
 
     return winners
 
-
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
-
+    while len(teams) > 1:
+        teams = simulate_round(teams)
+    return teams[0]["team"]
 
 if __name__ == "__main__":
     main()
