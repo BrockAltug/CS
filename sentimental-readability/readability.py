24a8d3145408ff130219348a16706772d294fc05
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-12 10:58:05 +0000

    Sat, Aug 12, 2023, 3:58 AM -07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,31 @@
-# TODO
+from cs50 import get_string
+
+def main():
+    text = get_string("Text: ")
+    letters, words, sentences = count_elements(text)
+    L = letters / words * 100
+    S = sentences / words * 100
+    index = compute_index(L, S)
+    grade_level = get_grade_level(index)
+    print(grade_level)
+
+def count_elements(text):
+    letters = sum(1 for char in text if char.isalpha())
+    words = len(text.split())
+    sentences = sum(1 for char in text if char in ['.', '!', '?'])
+    return letters, words, sentences
+
+def compute_index(L, S):
+    return 0.0588 * L - 0.296 * S - 15.8
+
+def get_grade_level(index):
+    rounded_index = round(index)
+    if rounded_index >= 16:
+        return "Grade 16+"
+    elif rounded_index < 1:
+        return "Before Grade 1"
+    else:
+        return f"Grade {rounded_index}"
+
+if __name__ == "__main__":
+    main()
