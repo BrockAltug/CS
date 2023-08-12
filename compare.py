1a5ab8c67e3aa63f9ab2c86e9e00ac301f418a92
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-12 02:35:27 +0000

    Fri, Aug 11, 2023, 7:35 PM -07:00

--- compare.py
+++ compare.py
@@ -1,3 +1,4 @@
+#Code written in C
 #// Conditionals, Boolean expressions, relational operators
 #
 #include <cs50.h>
@@ -23,3 +24,19 @@
 #        printf("x is equal to y\n");
 #    }
 #}
+#How the same thing is written in Python
+# Conditionals, Boolean expressions, relational operators
+
+from cs50 import get_int
+
+# Prompt user for integers
+x = get_int("What's x? ")
+y = get_int("What's y? ")
+
+# Compare integers
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
