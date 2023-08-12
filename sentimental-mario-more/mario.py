31bd9a6b244cac02afc264b5e5d11b001bac3c6b
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-12 10:44:17 +0000

    Sat, Aug 12, 2023, 3:44 AM -07:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,11 +1,19 @@
+# Main method
 def main():
+    # get the desired height of the half-pyramids from the user
     height = get_height()
+
+    # generate and print the half-pyramids with the provided height
     print_pyramids(height)
 
+# method to get a valid height input from the user
 def get_height():
     while True:
         try:
+            # prompt the user for input and convert it to an integer
             height = int(input("Height: "))
+
+            # check if the input is within the valid range (1 to 8)
             if 1 <= height <= 8:
                 return height
             else:
@@ -13,11 +21,17 @@ def get_height():
         except ValueError:
             print("Invalid input. Please enter a positive integer.")
 
+# method to print the half-pyramids based on the provided height
 def print_pyramids(height):
     for i in range(1, height + 1):
+        # calculate the number of spaces and hashes for each row
         spaces = " " * (height - i)
         hashes = "#" * i
+
+        # print the current row with appropriate formatting
         print(f"{spaces}{hashes}  {hashes}")
 
+# check if the script is being run as the main program
 if __name__ == "__main__":
+    # call the main method to start the program
     main()
