690735b9c40ef4a665be77718c89c4a2385eb453
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-12 10:42:45 +0000

    Sat, Aug 12, 2023, 3:42 AM -07:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,1 +1,23 @@
-# TODO
+def main():
+    height = get_height()
+    print_pyramids(height)
+
+def get_height():
+    while True:
+        try:
+            height = int(input("Height: "))
+            if 1 <= height <= 8:
+                return height
+            else:
+                print("Height must be between 1 and 8, inclusive.")
+        except ValueError:
+            print("Invalid input. Please enter a positive integer.")
+
+def print_pyramids(height):
+    for i in range(1, height + 1):
+        spaces = " " * (height - i)
+        hashes = "#" * i
+        print(f"{spaces}{hashes}  {hashes}")
+
+if __name__ == "__main__":
+    main()
