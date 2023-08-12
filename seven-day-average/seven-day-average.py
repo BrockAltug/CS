1cbf8a0fdc805d0d87847b5f1367d661cb142871
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-12 09:56:00 +0000

    Sat, Aug 12, 2023, 2:56 AM -07:00

--- seven-day-average/seven-day-average.py
+++ seven-day-average/seven-day-average.py
@@ -1,7 +1,6 @@
 import csv
 import requests
 
-
 def main():
     # Read NYTimes Covid Database
     download = requests.get(
@@ -31,15 +30,35 @@ def main():
     # Print out 7-day averages for this week vs last week
     comparative_averages(new_cases, states)
 
-
 # TODO: Create a dictionary to store 14 most recent days of new cases by state
 def calculate(reader):
-    ...
-
+    new_cases = {}
+    for row in reader:
+        state = row["state"]
+        cases = int(row["cases"])
+        if state not in new_cases:
+            new_cases[state] = []
+        if len(new_cases[state]) >= 14:
+            new_cases[state].pop(0)  # Remove the oldest day's data
+        if len(new_cases[state]) > 0:
+            previous_cases = new_cases[state][-1]
+            new_cases[state].append(cases - previous_cases)
+        else:
+            new_cases[state].append(cases)
+    return new_cases
 
 # TODO: Calculate and print out seven day average for given state
 def comparative_averages(new_cases, states):
-    ...
+    for state in states:
+        cases = new_cases[state]
+        current_week = sum(cases[-7:]) / 7
+        previous_week = sum(cases[-14:-7]) / 7 if len(cases) >= 14 else 0
 
+        try:
+            percent_change = ((current_week - previous_week) / previous_week) * 100
+        except ZeroDivisionError:
+            percent_change = 0
+
+        print(f"{state} had a 7-day average of {int(current_week)} and a change of {int(percent_change)}%.")
 
 main()
