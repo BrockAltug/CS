ee5893c8ac306b5015db33a7b0e9224ce04c5256
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-12 09:15:06 +0000

    Sat, Aug 12, 2023, 2:15 AM -07:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,12 +1,21 @@
+# Main method
 def main():
+    # prompt the user for a greeting and remove leading/trailing whitespace,
+    # then convert it to lowercase
     greeting = input("Enter a greeting: ").strip().lower()
 
+    # check if the greeting starts with "hello" and output $0 if true
     if greeting.startswith("hello"):
         print("$0")
+    # check if the greeting starts with "h" (but not "hello") and output $20 if true
     elif greeting.startswith("h"):
         print("$20")
+    # if neither of the above conditions is met, output $100
     else:
         print("$100")
 
+
+# check if the script is being run as the main program
 if __name__ == "__main__":
+    # call the main function
     main()
