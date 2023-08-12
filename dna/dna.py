0136a8a6464bafa2cc7528c65d951b2b919e8440
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-12 11:07:12 +0000

    Sat, Aug 12, 2023, 4:07 AM -07:00

--- dna/dna.py
+++ dna/dna.py
@@ -1,58 +1,57 @@
 import csv
 import sys
 
-
-def main():
-
-    # TODO: Check for command-line usage
-
-    # TODO: Read database file into a variable
-
-    # TODO: Read DNA sequence file into a variable
-
-    # TODO: Find longest match of each STR in DNA sequence
-
-    # TODO: Check database for matching profiles
-
-    return
-
-
 def longest_match(sequence, subsequence):
     """Returns length of longest run of subsequence in sequence."""
-
-    # Initialize variables
     longest_run = 0
     subsequence_length = len(subsequence)
     sequence_length = len(sequence)
 
-    # Check each character in sequence for most consecutive runs of subsequence
     for i in range(sequence_length):
-
-        # Initialize count of consecutive runs
         count = 0
 
-        # Check for a subsequence match in a "substring" (a subset of characters) within sequence
-        # If a match, move substring to next potential match in sequence
-        # Continue moving substring and checking for matches until out of consecutive matches
         while True:
-
-            # Adjust substring start and end
             start = i + count * subsequence_length
             end = start + subsequence_length
 
-            # If there is a match in the substring
             if sequence[start:end] == subsequence:
                 count += 1
-
-            # If there is no match in the substring
             else:
                 break
 
-        # Update most consecutive matches found
         longest_run = max(longest_run, count)
 
-    # After checking for runs at each character in seqeuence, return longest run found
     return longest_run
 
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python dna.py data.csv sequence.txt")
+        return
 
-main()
+    csv_filename = sys.argv[1]
+    individuals = []
+    with open(csv_filename, newline='') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            individuals.append(row)
+
+    dna_filename = sys.argv[2]
+    with open(dna_filename, 'r') as dna_file:
+        dna_sequence = dna_file.read()
+
+    for individual in individuals:
+        name = individual['name']
+        str_counts = {key: int(value) for key, value in individual.items() if key != 'name'}
+        match = True
+        for str_name, str_count in str_counts.items():
+            if longest_match(dna_sequence, str_name) != str_count:
+                match = False
+                break
+        if match:
+            print(name)
+            return
+
+    print("No match")
+
+if __name__ == "__main__":
+    main()
