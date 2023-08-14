081663b5b49ddb5def5b36e93e42dc2cba9fea92
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-14 05:04:18 +0000

    Sun, Aug 13, 2023, 10:04 PM -07:00

--- favorites.py
+++ favorites.py
@@ -239,3 +239,25 @@ with open("favorites.csv", "r") as file:
 favorite = input("Favorite: ")
 if favorite in counts:
     print(f"{favorite}: {counts[favorite]}")
+#----------------------------------------------------
+print("----------------------------------------------")
+    # Searches database popularity of a problem
+
+import csv
+
+from cs50 import SQL
+
+# Open database
+db = SQL("sqlite:///favorites.db")
+
+# Prompt user for favorite
+favorite = input("Favorite: ")
+
+# Search for title
+rows = db.execute("SELECT COUNT(*) FROM favorites WHERE problem LIKE ?", "%" + favorite + "%")
+
+# Get first (and only) row
+row = rows[0]
+
+# Print popularity
+print(row["COUNT(*)"])
