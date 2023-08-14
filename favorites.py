02885748edc0a94cf49510ace2efb25af31accb1
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-14 04:04:19 +0000

    Sun, Aug 13, 2023, 9:04 PM -07:00

--- favorites.py
+++ favorites.py
@@ -77,6 +77,56 @@
 #---------------------------------------------------------------
 # Counts favorites using dictionary
 
+#import csv
+
+# Open CSV file
+#with open("favorites.csv", "r") as file:
+
+    # Create DictReader
+#    reader = csv.DictReader(file)
+
+    # Counts
+#    counts = {}
+
+    # Iterate over CSV file, counting favorites
+#    for row in reader:
+#        favorite = row["language"]
+#        if favorite in counts:
+#            counts[favorite] += 1
+#        else:
+#            counts[favorite] = 1
+
+# Print counts
+#for favorite in counts:
+#    print(f"{favorite}: {counts[favorite]}")
+#--------------------------------------------------------------
+# Sorts favorites by key
+
+#import csv
+
+# Open CSV file
+#with open("favorites.csv", "r") as file:
+
+    # Create DictReader
+#    reader = csv.DictReader(file)
+
+    # Counts
+#    counts = {}
+
+    # Iterate over CSV file, counting favorites
+#    for row in reader:
+#        favorite = row["language"]
+#        if favorite in counts:
+#            counts[favorite] += 1
+#        else:
+#            counts[favorite] = 1
+
+# Print counts
+#for favorite in sorted(counts):
+#    print(f"{favorite}: {counts[favorite]}")
+#--------------------------------------------------------------------
+# Sorts favorites by value
+
 import csv
 
 # Open CSV file
@@ -96,6 +146,9 @@ with open("favorites.csv", "r") as file:
         else:
             counts[favorite] = 1
 
+def get_value(language):
+    return counts[language]
+
 # Print counts
-for favorite in counts:
+for favorite in sorted(counts, key=get_value, reverse=True):
     print(f"{favorite}: {counts[favorite]}")
