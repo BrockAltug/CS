d6578f7daab3289531194f4e5593d0878c796e62
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-16 09:14:53 +0000

    Wed, Aug 16, 2023, 2:14 AM -07:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -6,3 +6,6 @@
 #SELECT student_name, house FROM students WHERE student_name LIKE "%Potter" --searches for any character similar before Potter
 #SELECT student_name, house FROM students WHERE student_name LIKE "Harry%" --searches for any characters similar after Harry
 #SELECT * FROM students ORDER BY house, student_name; --orders the list of students first by their house and then by the student name in alphabetical order
+#SELECT COUNT(*) FROM students; --displays the number of rows
+#SELECT COUNT(*) AS number_of_students FROM students; --assasigns count as number_of_students
+#SELECT COUNT(*) FROM students WHERE house IS 'Slytherin'; --displays amount of students in the specified house
