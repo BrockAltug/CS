ef2be3f2b8501a02862acba20ed3595cef76e1fd
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-16 07:18:55 +0000

    Wed, Aug 16, 2023, 12:18 AM -07:00

--- shows.py
+++ shows.py
@@ -35,3 +35,8 @@
 #CREATE INDEX title_index on shows (title);        //creates new index
 #SELECT * FROM shows WHERE title = 'The Office';   //this now runs much more quickly
 #Unfortunately, indexing all columns would result in utilizing more storage space. Therefore, there is a tradeoff for enhanced speed.
+#--------------------------------------------------
+#Utilization of SQL can sometimes result in some problems.
+#You can imagine a case where multiple users could be accessing the same database and executing commands at the same time.
+#This could result in glitches where code is interrupted by other people’s actions. This could result in a loss of data.
+#Built-in SQL features such as BEGIN TRANSACTION, COMMIT, and ROLLBACK help avoid some of these race condition problems.
