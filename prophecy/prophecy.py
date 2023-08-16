5bde800d1cca6dead8378d238d0b5f575de5b1e7
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-16 08:51:46 +0000

    Wed, Aug 16, 2023, 1:51 AM -07:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -3,3 +3,5 @@
 #.schema
 #SELECT * FROM students;
 #SELECT student_name, house FROM students WHERE house IS 'Slytherin';
+#SELECT student_name, house FROM students WHERE student_name LIKE "%Potter" --searches for any character similar before Potter
+##SELECT student_name, house FROM students WHERE student_name LIKE "Harry%" --searches for any characters similar after Harry
