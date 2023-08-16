9d54571eff20bcdce10d6ca959dbf6aa83c61918
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-16 09:01:43 +0000

    Wed, Aug 16, 2023, 2:01 AM -07:00

--- prophecy/prophecy.py
+++ prophecy/prophecy.py
@@ -4,4 +4,5 @@
 #SELECT * FROM students;
 #SELECT student_name, house FROM students WHERE house IS 'Slytherin';
 #SELECT student_name, house FROM students WHERE student_name LIKE "%Potter" --searches for any character similar before Potter
-##SELECT student_name, house FROM students WHERE student_name LIKE "Harry%" --searches for any characters similar after Harry
+#SELECT student_name, house FROM students WHERE student_name LIKE "Harry%" --searches for any characters similar after Harry
+#SELECT * FROM students ORDER BY house, student_name; --orders the list of students first by their house and then by the student name in alphabetical order
