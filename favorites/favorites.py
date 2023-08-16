46df0fec22008eed931d749786b6fd1e6dc0b544
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-16 08:29:56 +0000

    Wed, Aug 16, 2023, 1:29 AM -07:00

---
+++ favorites/favorites.py
@@ -0,0 +1,6 @@
+#cd ./favorites --opens the favorites directory
+#sqlite3 favorites.db --access SQL database
+#schema --view available tables
+
+#UPDATE shows SET title = 'Adventure Time' WHERE title LIKE 'adventure time%'; --updates the show name if there are any similarities LIKE
+#SELECT DISTINCT title FROM shows; --shows unique titles from the shows table
