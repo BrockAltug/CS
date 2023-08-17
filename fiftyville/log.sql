ce7c61eaca09abe32841a3e0d5190375ddaf827a
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-17 06:12:01 +0000

    Wed, Aug 16, 2023, 11:12 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -11,6 +11,7 @@ SELECT * FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 --(2. before going to bakery thief withdrew money from LEGGET STREET atm)
 SELECT * FROM atm_transactions WHERE atm_location = 'Leggett Street' AND year = 2021 AND month = 7 AND day = 28; --account number
 --(3. thief made a phone call less than 1 minute after the robbery as they left bakery planning to take earliest flight out of the city the next day)
+SELECT * FROM bank_accounts WHERE account_number = 28500762;
 -- Find information about the accomplice based on interview taking the earliest flight out of Fiftyville the next day, and asked other person to purchase a flight ticket
 SELECT * FROM flights WHERE month = 7 AND day = 29;
 
