e1af5e4eb6aeb961c3075419ee86e3ac23d7fdec
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-17 07:19:24 +0000

    Thu, Aug 17, 2023, 12:19 AM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
+--cd fiftyville
+--sqlite3 fiftyville.db
+--cat log.sql | sqlite3 fiftyville.db
+
 
 --find the date the crime took place and select description from crime scene report --was stolen from the Humphrey Street bakery at 10:15am
 SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
@@ -17,6 +21,28 @@ AND bakery_security_logs.minute >= 15
 AND bakery_security_logs.minute <= 25;
 
 --8 possible suspects
+
+--get the peoples name and atm transactions type from people, joining bank accounts on person id and atm transactions on account number searching for the date and location from witnesses
+SELECT people.name, atm_transactions.transaction_type FROM people
+JOIN bank_accounts ON bank_accounts.person_id = people.id
+JOIN atm_transactions ON atm_transactions.account_number =
+bank_accounts.account_number
+WHERE atm_transactions.year = 2021
+AND atm_transactions.month = 7
+AND atm_transactions.day = 28
+AND atm_location = "Leggett Street"
+AND atm_transactions.transaction_type = "withdraw";
+
+--witness says phone call for less than 1 minute, earliest flight the next day. other person on phone to purchase the flight ticket
+--find caller, reciever phone call less than 1 minute on the day
+SELECT caller, receiver FROM phone_calls
+WHERE year = 2021
+AND month = 7
+AND day = 28
+AND duration < 60;
+
+
+
 
 
 
