c8ace2675364d3a9ea1cfb4db6835305c091a84e
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-17 05:14:23 +0000

    Wed, Aug 16, 2023, 10:14 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Examine crime_scene_reports table to find the relevant crime scene report
+--SELECT * FROM crime_scene_reports WHERE year = <year> AND month = <month> AND day = <day> AND street = '<street>';
+
+-- Look for interviews conducted on the same date as the crime
+--SELECT * FROM interviews WHERE year = <year> AND month = <month> AND day = <day>;
+
