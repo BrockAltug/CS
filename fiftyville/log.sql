d1c0d4296da890bb26dce90bdad889134458f382
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-17 06:05:03 +0000

    Wed, Aug 16, 2023, 11:05 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -6,7 +6,11 @@ SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 A
 -- Find interviews from witnesses for the specified crime scene report
 SELECT * FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
 
--- Find information about the accomplice based on interview taking the earliest flight out of Fiftyville the next day
+--3 witnesses say
+--(1. within 10 minutes of the theft, the thief got into a car in the bakery parking lot)
+--(2. before going to bakery thief withdrew money from LEGGET STREET atm)
+--(3. thief made a phone call less than 1 minute after the robbery as they left bakery planning to take earliest flight out of the city the next day)
+-- Find information about the accomplice based on interview taking the earliest flight out of Fiftyville the next day, and asked other person to purchase a flight ticket
 SELECT * FROM flights WHERE month = 7 AND day = 29;
 
 
