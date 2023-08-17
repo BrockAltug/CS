c1aebad743b208da68653ff66aa6abb1baf5fd43
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-17 05:34:23 +0000

    Wed, Aug 16, 2023, 10:34 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -3,8 +3,8 @@
 -- Find the crime scene report for the given date and location
 SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
 
--- Find witnesses for the specified crime scene report
-SELECT * FROM witnesses WHERE crime_id = 295;
+-- Find interviews from witnesses for the specified crime scene report
+SELECT * FROM interviews WHERE crime_id = 295;
 
 -- Find information about the accomplice
 --SELECT * FROM accomplices WHERE name = '<accomplice_name>';
