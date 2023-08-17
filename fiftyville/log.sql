49facfbce97f70bd20dca6ad7d1d895c509e510d
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-17 07:07:35 +0000

    Thu, Aug 17, 2023, 12:07 AM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,59 +1,18 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
---find the date the crime took place
-SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
+--find the date the crime took place and select description from crime scene report --was stolen from the bakery
+SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
 
---find interviews from witnesses on the date the crime happened
-SELECT * FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
+--get interviews from people on the date of the crime related to the bakery
+SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE '%bakery%';
 
---gets all atm transactions at the location from witnesses (witness says theif will board earliest plane the next day and accessed ATM)
-SELECT * FROM atm_transactions WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = 'Leggett Street';
+--interview says within 10 mins thief took a car and left the parking lot --bakery security logs
+SELECT bakery_security_logs.activity, bakery_security_logs.license_plate, people.name FROM people
+JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+Where bakery_security_logs.year = 2021;
 
---gets all earliest flights leaving the next day
-SELECT * FROM flights WHERE year = 2021 AND month = 7 AND day = 29 ORDER BY hour, minute LIMIT 1;
 
---gets all of the passanger information
-SELECT p.name, p.phone_number, ps.seat FROM passengers AS ps JOIN people AS p ON ps.passport_number = p.passport_number WHERE ps.flight_id = 36;
 
---Retrieve license plate from the people where phone number is matching from flights
-SELECT license_plate FROM people WHERE phone_number = '(066) 555-9701'; --M51FA04 license plates
-SELECT license_plate FROM people WHERE phone_number = '(130) 555-0289'; --G412CB7
-SELECT license_plate FROM people WHERE phone_number = '(367) 555-5533'; --94KL13X
-SELECT license_plate FROM people WHERE phone_number = '(328) 555-1152'; --130LD9Z
-SELECT license_plate FROM people WHERE phone_number = '(499) 555-9472'; --0NTHK55
-SELECT license_plate FROM people WHERE phone_number = '(286) 555-6063'; --1106N58
-SELECT license_plate FROM people WHERE phone_number = '(389) 555-5198'; --4328GD8
-
---To retrieve security logs from the bakery security table where the day, month, year, and license plate match
-SELECT * FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND license_plate = 'M51FA04'; --did not go to bakery
-SELECT * FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND license_plate = 'G412CB7';
-SELECT * FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND license_plate = '94KL13X';
-SELECT * FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND license_plate = '130LD9Z'; -- did not go
-SELECT * FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND license_plate = '0NTHK55';
-SELECT * FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND license_plate = '1106N58';
-SELECT * FROM bakery_security_logs WHERE day = 28 AND month = 7 AND year = 2021 AND license_plate = '4328GD8';
-
---thief made a phone call for less than 1 minute
-SELECT * FROM phone_calls WHERE ((caller = '(066) 555-9701' AND year = 2021 AND month = 7 AND day = 28) OR (receiver = '(066) 555-9701' AND year = 2021 AND month = 7 AND day = 28)) AND duration < 60;
-SELECT * FROM phone_calls WHERE ((caller = '(130) 555-0289' AND year = 2021 AND month = 7 AND day = 28) OR (receiver = '(130) 555-0289' AND year = 2021 AND month = 7 AND day = 28)) AND duration < 60;
-SELECT * FROM phone_calls WHERE ((caller = '(367) 555-5533' AND year = 2021 AND month = 7 AND day = 28) OR (receiver = '(367) 555-5533' AND year = 2021 AND month = 7 AND day = 28)) AND duration < 60;
-SELECT * FROM phone_calls WHERE ((caller = '(328) 555-1152' AND year = 2021 AND month = 7 AND day = 28) OR (receiver = '(328) 555-1152' AND year = 2021 AND month = 7 AND day = 28)) AND duration < 60; --didnt make a call at this time
-SELECT * FROM phone_calls WHERE ((caller = '(499) 555-9472' AND year = 2021 AND month = 7 AND day = 28) OR (receiver = '(499) 555-9472' AND year = 2021 AND month = 7 AND day = 28)) AND duration < 60;
-SELECT * FROM phone_calls WHERE ((caller = '(286) 555-6063' AND year = 2021 AND month = 7 AND day = 28) OR (receiver = '(286) 555-6063' AND year = 2021 AND month = 7 AND day = 28)) AND duration < 60;
-SELECT * FROM phone_calls WHERE ((caller = '(389) 555-5198' AND year = 2021 AND month = 7 AND day = 28) OR (receiver = '(389) 555-5198' AND year = 2021 AND month = 7 AND day = 28)) AND duration < 60; --didnt make a call at this time
-
---get license plates from updated suspects
-SELECT license_plate FROM people WHERE phone_number = '(286) 555-6063'; --1106N58
-SELECT license_plate FROM people WHERE phone_number = '(499) 555-9472'; --0NTHK55
-SELECT license_plate FROM people WHERE phone_number = '(367) 555-5533'; --94KL13X
-SELECT license_plate FROM people WHERE phone_number = '(130) 555-0289'; --G412CB7
-SELECT license_plate FROM people WHERE phone_number = '(066) 555-9701'; --M51FA04
-
-SELECT name FROM people WHERE license_plate = '1106N58';
-SELECT name FROM people WHERE license_plate = '0NTHK55';
-SELECT name FROM people WHERE license_plate = '94KL13X';
-SELECT name FROM people WHERE license_plate = 'G412CB7';
-SELECT name FROM people WHERE license_plate = 'M51FA04'; -- did not goto bakery
 
 
 
