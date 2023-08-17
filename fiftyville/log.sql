72f1d4609c19d77216272f607487046d7f3dbcf8
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-17 05:27:33 +0000

    Wed, Aug 16, 2023, 10:27 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,8 +1,13 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- Examine crime_scene_reports table to find the relevant crime scene report
---SELECT * FROM crime_scene_reports WHERE year = <year> AND month = <month> AND day = <day> AND street = '<street>';
+-- Find the crime scene report for the given date and location
+--SELECT * FROM crime_scene_reports WHERE year = <year> AND month = <month> AND day = <day> AND street = '<street_name>';
 
--- Look for interviews conducted on the same date as the crime
---SELECT * FROM interviews WHERE year = <year> AND month = <month> AND day = <day>;
+-- Find witnesses for the specified crime scene report
+--SELECT * FROM witnesses WHERE crime_id = <crime_id>;
+
+-- Find information about the accomplice
+--SELECT * FROM accomplices WHERE name = '<accomplice_name>';
+
+
 
