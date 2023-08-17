f554b86fff8fc203f5b63e1966a425ac12fb1f8d
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-17 06:20:59 +0000

    Wed, Aug 16, 2023, 11:20 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,27 +1,10 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
--- Find the crime scene report for the given date and location
 SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
 
--- Find interviews from witnesses for the specified crime scene report
-SELECT * FROM interviews WHERE year = 2021 AND month = 7 AND day = 28;
+SELECT * FROM interviews WHERE id = REPORT_ID;
 
---3 witnesses say
---(1. within 10 minutes of the theft, the thief got into a car in the bakery parking lot)
---(2. before going to bakery thief withdrew money from LEGGET STREET atm)
-SELECT * FROM atm_transactions WHERE atm_location = 'Leggett Street' AND year = 2021 AND month = 7 AND day = 28; --account number
-SELECT * FROM bank_accounts WHERE account_number = 28500762;
-SELECT * FROM bank_accounts WHERE account_number = 28296815;
-SELECT * FROM bank_accounts WHERE account_number = 76054385;
-SELECT * FROM bank_accounts WHERE account_number = 49610011;
-SELECT * FROM bank_accounts WHERE account_number = 16153065;
-SELECT * FROM bank_accounts WHERE account_number = 86363979;
-SELECT * FROM bank_accounts WHERE account_number = 25506511;
-SELECT * FROM bank_accounts WHERE account_number = 81061156;
-SELECT * FROM bank_accounts WHERE account_number = 26013199;
---(3. thief made a phone call less than 1 minute after the robbery as they left bakery planning to take earliest flight out of the city the next day)
--- Find information about the accomplice based on interview taking the earliest flight out of Fiftyville the next day, and asked other person to purchase a flight ticket
-SELECT * FROM flights WHERE month = 7 AND day = 29 AND person_id =28500762;
+
 
 
 
