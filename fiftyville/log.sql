d025fd334c86c053c4920d61d383398e9a9bbaa8
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-17 05:31:10 +0000

    Wed, Aug 16, 2023, 10:31 PM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,7 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
 -- Find the crime scene report for the given date and location
---SELECT * FROM crime_scene_reports WHERE year = <year> AND month = <month> AND day = <day> AND street = '<street_name>';
+--SELECT * FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
 
 -- Find witnesses for the specified crime scene report
 --SELECT * FROM witnesses WHERE crime_id = <crime_id>;
