b8bdaa2d174c3b30f7cdc580064ee188f50f99bc
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-17 08:13:20 +0000

    Thu, Aug 17, 2023, 1:13 AM -07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,78 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Keep a log of any SQL queries you execute as you solve the mystery.
+
+--cd fiftyville
+--sqlite3 fiftyville.db
+--cat log.sql | sqlite3 fiftyville.db
+
+
+--find the date the crime took place and select description from crime scene report --was stolen from the Humphrey Street bakery at 10:15am
+SELECT description FROM crime_scene_reports WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
+
+--get interviews from people on the date of the crime related to the bakery
+SELECT transcript FROM interviews WHERE year = 2021 AND month = 7 AND day = 28 AND transcript LIKE '%bakery%';
+
+--interview says within 10 mins thief took a car and left the parking lot --bakery security logs retrieves activities, license plates, and names from security logs
+SELECT bakery_security_logs.activity, bakery_security_logs.license_plate, people.name FROM people
+JOIN bakery_security_logs ON bakery_security_logs.license_plate = people.license_plate
+Where bakery_security_logs.year = 2021
+AND bakery_security_logs.month = 7
+AND bakery_security_logs.day = 28
+AND bakery_security_logs.hour = 10
+AND bakery_security_logs.minute >= 15
+AND bakery_security_logs.minute <= 25;
+
+--8 possible suspects
+
+--get the peoples name and atm transactions type from people, joining bank accounts on person id and atm transactions on account number searching for the date and location from witnesses
+SELECT people.name, atm_transactions.transaction_type FROM people
+JOIN bank_accounts ON bank_accounts.person_id = people.id
+JOIN atm_transactions ON atm_transactions.account_number =
+bank_accounts.account_number
+WHERE atm_transactions.year = 2021
+AND atm_transactions.month = 7
+AND atm_transactions.day = 28
+AND atm_location = "Leggett Street"
+AND atm_transactions.transaction_type = "withdraw";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
