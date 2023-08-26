0a87a41a32b0f4877e88314c2e800c38dcf56f99
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-26 03:33:29 +0000

    Fri, Aug 25, 2023, 8:33 PM -07:00

--- hello5.js
+++ hello5.js
@@ -1,9 +1,12 @@
+// Add an event listener to execute code when the DOM content is loaded
 document.addEventListener('DOMContentLoaded', function(){
-
+    // Add an event listener to the form's submit event
     document.querySelector('form').addEventListener('submit', funtion(event){
-
+        // Retrieve the value entered in the 'name' field
         let name = document.querySelector('#name'.value);
+        // Display an alert message using the retrieved name
         alert('hello, ' + name);
+        // Prevent the form from performing its default submission behavior
         event.preventDefault();
     });
 });
