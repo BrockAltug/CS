b0b16efdeebc9c75908e6e78c96f9c8e3f7c4f95
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-31 08:48:19 +0000

    Thu, Aug 31, 2023, 1:48 AM -07:00

--- flaskstore/app.py
+++ flaskstore/app.py
@@ -13,41 +13,27 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 @app.route("/")
 def index():
-    books = [
-        {"id": 1, "title": "Book 1", "author": "Author 1", "price": 10.99},
-        {"id": 2, "title": "Book 2", "author": "Author 2", "price": 12.99},
-        {"id": 3, "title": "Book 3", "author": "Author 3", "price": 8.99},
-    ]
+    books = db.execute("SELECT * FROM books")
     return render_template("books.html", books=books)
+
 
 @app.route("/cart", methods=["GET", "POST"])
 def cart():
+
+    # Ensure cart exists
     if "cart" not in session:
         session["cart"] = []
 
+    # POST
     if request.method == "POST":
         id = request.form.get("id")
         if id:
             session["cart"].append(id)
         return redirect("/cart")
 
-    book_ids = session["cart"]
-    books = []
-    total = 0
-    if book_ids:
-        books = db.execute("SELECT * FROM books WHERE id IN (?)", book_ids)
-        total = sum(book["price"] for book in books)
-
-    return render_template("cart.html", books=books, total=total)
-
-@app.route("/remove", methods=["POST"])
-def remove_from_cart():
-    id = request.form.get("id")
-    if id in session["cart"]:
-        session["cart"].remove(id)
-    return redirect("/cart")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    # GET
+    books = db.execute("SELECT * FROM books WHERE id IN (?)", session["cart"])
+    return render_template("cart.html", books=books)
