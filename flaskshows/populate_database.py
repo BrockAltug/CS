6cb42573102d5cb234500b2876adb00c64042287
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-31 09:27:18 +0000

    Thu, Aug 31, 2023, 2:27 AM -07:00

--- flaskshows/populate_database.py
+++ flaskshows/populate_database.py
@@ -1,5 +1,4 @@
 import sqlite3
-import random
 
 # Connect to the database (will create if not exists)
 conn = sqlite3.connect('shows.db')
@@ -14,20 +13,40 @@ cursor.execute('''
     )
 ''')
 
-# Generate random show data
-def generate_random_show():
-    show_title = f"Show {random.randint(1, 100)}"
-    show_year = random.randint(1980, 2023)
-    return (None, show_title, show_year)
+# Sample show data
+shows_data = [
+    (1, 'Stranger Things', 2016),
+    (2, 'Game of Thrones', 2011),
+    (3, 'Breaking Bad', 2008),
+    (4, 'Friends', 1994),
+    (5, 'The Office', 2005),
+    (6, 'The Simpsons', 1989),
+    (7, 'Black Mirror', 2011),
+    (8, 'The Crown', 2016),
+    (9, 'Sherlock', 2010),
+    (10, 'Fargo', 2014),
+    (11, 'The Mandalorian', 2019),
+    (12, 'The Big Bang Theory', 2007),
+    (13, 'Stranger Things', 2016),
+    (14, 'Chernobyl', 2019),
+    (15, 'Friends', 1994),
+    (16, 'Money Heist', 2017),
+    (17, 'Breaking Bad', 2008),
+    (18, 'The Witcher', 2019),
+    (19, 'Parks and Recreation', 2009),
+    (20, 'Brooklyn Nine-Nine', 2013),
+    (21, 'Rick and Morty', 2013),
+    (22, 'Westworld', 2016),
+    (23, 'The Office', 2005),
+    (24, 'The Crown', 2016),
+    (25, 'Narcos', 2015)
+]
 
-# Generate 25 random shows
-random_shows = [generate_random_show() for _ in range(25)]
-
-# Insert random shows into the table
-cursor.executemany('INSERT INTO shows (id, title, year) VALUES (?, ?, ?)', random_shows)
+# Insert sample data into the table
+cursor.executemany('INSERT INTO shows (id, title, year) VALUES (?, ?, ?)', shows_data)
 
 # Commit changes and close the connection
 conn.commit()
 conn.close()
 
-print("Database 'shows.db' created and populated with random show data.")
+print("Database 'shows.db' created and populated with manual show data.")
