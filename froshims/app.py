113b82ed6fb9027890b2fdcfac67e3e5e049d2d8
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-31 07:22:37 +0000

    Thu, Aug 31, 2023, 12:22 AM -07:00

--- froshims/app.py
+++ froshims/app.py
@@ -1,29 +1,44 @@
-from flask import Flask, render_template, request
+# Implements a registration form, storing registrants in a dictionary, with error messages
+
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
 REGISTRANTS = {}
-#SPORTS created so users can not create their own category from inspect element, for security
+
 SPORTS = [
     "Basketball",
     "Soccer",
-    "Football",
+    "Ultimate Frisbee"
 ]
+
 
 @app.route("/")
 def index():
     return render_template("index.html", sports=SPORTS)
 
+
 @app.route("/register", methods=["POST"])
 def register():
+
+    # Validate name
     name = request.form.get("name")
     if not name:
-        return render_template("failure.html")
+        return render_template("error.html", message="Missing name")
+
+    # Validate sport
     sport = request.form.get("sport")
+    if not sport:
+        return render_template("error.html", message="Missing sport")
     if sport not in SPORTS:
-        return render_template("failure.html")
+        return render_template("error.html", message="Invalid sport")
+
+    # Remember registrant
     REGISTRANTS[name] = sport
-    return render_template("success.html")
+
+    # Confirm registration
+    return redirect("/registrants")
+
 
 @app.route("/registrants")
 def registrants():
