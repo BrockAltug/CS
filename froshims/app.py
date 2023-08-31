82af1f65da7ce8d7f75be6dd04a07e60f0a7442a
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-31 06:22:46 +0000

    Wed, Aug 30, 2023, 11:22 PM -07:00

--- froshims/app.py
+++ froshims/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-registrants = {}
+registrants = {} #dictionary
 
 @app.route("/")
 def index():
@@ -10,5 +10,8 @@ def index():
 
 @app.route("/register", methods=["POST"])
 def register():
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+    registrants[name] = sport
     return render_template("success.html")
 
