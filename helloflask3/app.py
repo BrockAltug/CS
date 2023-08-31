efcf95cd3d488287006441cb9601a625c36569de
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-31 05:47:11 +0000

    Wed, Aug 30, 2023, 10:47 PM -07:00

--- helloflask3/app.py
+++ helloflask3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request     #has 3 functions imported from Flask framework               #examples with GET / POST
+from flask import Flask, render_template, request     #has 3 functions imported from Flask framework               #examples with GET / POST        and request.form for post
 
 app = Flask(__name__)   #turns the file into a Flask application
 
@@ -14,4 +14,4 @@ def index():
     if request.method == "GET":
          return render_template("index.html") #, placeholder=name) #placeholder changed to name variable
     elif request.method == "POST":
-        return render_template("greet.html", name=request.args.get("name", "world"))
+        return render_template("greet.html", name=request.form.get("name", "world"))
