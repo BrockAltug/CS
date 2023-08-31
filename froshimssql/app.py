821f60a2fb3901e0bf9a715a038043629850fe26
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-08-31 07:14:06 +0000

    Thu, Aug 31, 2023, 12:14 AM -07:00

--- froshimssql/app.py
+++
@@ -1,9 +0,0 @@
-#implements a registration form, storing registrants in a sqlite database
-
-from cs50 import SQL
-from flask import Flask, redirect, render_template, request
-
-app = Flask(__name__)
-
-db = SQL("sqlite://froshims.db")
-
