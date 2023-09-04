5d9319e84b9d1a021c9056f8055564851241f2f9
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-09-04 08:11:35 +0000

    Mon, Sep 4, 2023, 1:11 AM -07:00

--- finance/app.py
+++ finance/app.py
@@ -16,6 +16,7 @@ app = Flask(__name__)
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
+
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
