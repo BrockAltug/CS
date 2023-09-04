4165fe11abfffec8b5f0bfa435ab249655d83ba5
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-09-04 07:18:54 +0000

    Mon, Sep 4, 2023, 12:18 AM -07:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -1,5 +1,5 @@
+# Import necessary modules
 import os
-
 from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 
@@ -12,30 +12,30 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///birthdays.db")
 
-
+# Ensure responses aren't cached
 @app.after_request
 def after_request(response):
-    """Ensure responses aren't cached"""
     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
 
-
+# Route for both GET and POST requests
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # TODO: Add the user's entry into the database
+        # Get data from the form
         name = request.form.get("name")
         month = request.form.get("month")
         day = request.form.get("day")
 
-        # Insert the entry into the database (implement this)
+        # Insert the data into the database
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
 
     # Fetch all entries from the database
     entries = db.execute("SELECT * FROM birthdays")
 
     return render_template("index.html", entries=entries)
 
-
-
+if __name__ == "__main__":
+    app.run()
