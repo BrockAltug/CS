dabb60126a9e172eb2aca9e2eaad509361d75d7e
BrockAltug <124009457+BrockAltug@users.noreply.github.com>
2023-09-04 09:07:57 +0000

    Mon, Sep 4, 2023, 2:07 AM -07:00

--- finance/app.py
+++ finance/app.py
@@ -21,16 +21,22 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///finance.db")
 
+
 @app.route("/")
 @login_required
 def index():
     """Show portfolio of stocks"""
 
     # Query database for user's portfolio
-    rows = db.execute("SELECT symbol, SUM(shares) AS total_shares FROM transactions WHERE user_id = ? GROUP BY symbol HAVING total_shares > 0", session["user_id"])
+    rows = db.execute(
+        "SELECT symbol, SUM(shares) AS total_shares FROM transactions WHERE user_id = ? GROUP BY symbol HAVING total_shares > 0",
+        session["user_id"],
+    )
 
     # Initialize variables
-    cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0]["cash"]
+    cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])[0][
+        "cash"
+    ]
     portfolio = []
     total_value = cash
 
@@ -40,9 +46,19 @@ def index():
         total_shares = row["total_shares"]
         total_stock_value = stock["price"] * total_shares
         total_value += total_stock_value
-        portfolio.append({"symbol": stock["symbol"], "name": stock["name"], "shares": total_shares, "price": usd(stock["price"]), "total": usd(total_stock_value)})
+        portfolio.append(
+            {
+                "symbol": stock["symbol"],
+                "name": stock["name"],
+                "shares": total_shares,
+                "price": usd(stock["price"]),
+                "total": usd(total_stock_value),
+            }
+        )
 
-    return render_template("index.html", portfolio=portfolio, cash=usd(cash), total=usd(total_value))
+    return render_template(
+        "index.html", portfolio=portfolio, cash=usd(cash), total=usd(total_value)
+    )
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -54,7 +70,6 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
         # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
@@ -64,10 +79,14 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
@@ -79,7 +98,6 @@ def login():
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
-
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -111,7 +129,9 @@ def buy():
 
         # Ensure the user has enough cash for the purchase
         user_id = session["user_id"]
-        user_cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)[0]["cash"]
+        user_cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)[0][
+            "cash"
+        ]
         if user_cash < total_cost:
             return apology("not enough cash to make the purchase", 400)
 
@@ -134,7 +154,9 @@ def buy():
         # Display the buy form
         return render_template("buy.html")
 
+
 # Implement other routes for quote, sell, history, and registration here...
+
 
 @app.route("/quote", methods=["GET", "POST"])
 @login_required
@@ -156,6 +178,7 @@ def quote():
 
     else:
         return render_template("quote.html")
+
 
 @app.route("/sell", methods=["GET", "POST"])
 @login_required
@@ -204,7 +227,9 @@ def sell():
         )
 
         # Update the user's cash balance
-        db.execute("UPDATE users SET cash = cash + ? WHERE id = ?", total_value, user_id)
+        db.execute(
+            "UPDATE users SET cash = cash + ? WHERE id = ?", total_value, user_id
+        )
 
         # Redirect to the portfolio page
         return redirect("/")
@@ -225,12 +250,11 @@ def history():
     # Query the transaction history for the logged-in user
     transactions = db.execute(
         "SELECT symbol, shares, price, datetime FROM transactions WHERE user_id = ? ORDER BY datetime DESC",
-        session["user_id"]
+        session["user_id"],
     )
 
     # Render the "history.html" template with the transaction history data
     return render_template("history.html", transactions=transactions)
-
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -255,13 +279,19 @@ def register():
         hashed_password = generate_password_hash(password)
 
         # Insert the new user into the database
-        result = db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hashed_password)
+        result = db.execute(
+            "INSERT INTO users (username, hash) VALUES (?, ?)",
+            username,
+            hashed_password,
+        )
 
         if not result:
             return apology("username already exists")
 
         # Log in the new user
-        user_id = db.execute("SELECT id FROM users WHERE username = ?", username)[0]["id"]
+        user_id = db.execute("SELECT id FROM users WHERE username = ?", username)[0][
+            "id"
+        ]
         session["user_id"] = user_id
 
         # Redirect to the portfolio page
@@ -278,7 +308,6 @@ def logout():
     session.clear()
     # Redirect to the homepage
     return redirect("/")
-
 
 
 if __name__ == "__main__":
